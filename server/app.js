d00cbef8dbf0875bf04e0be398e25fa114d2c7cb
Kévin <kevin.philip@outlook.fr>
2019-11-23 15:11:32 +0100

    feature(v0) : simple chat application using socket.io and express

--- server/app.js
+++ server/app.js
@@ -1,10 +1,31 @@
-const express = require('express')
-const app = express()
+var app = require('express')();
+var path = require('path');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+var usercount = 0;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+io.on('connect', (socket) => {
+  socket.username = 'User'+ usercount;
+  usercount ++;
+  
+  socket.emit('message', 'You are ' + socket.username);
+  io.emit('message', socket.username + ' joined the channel.');
+  
+  socket.on('message', (msg) => {
+    io.emit('message', socket.username + ' said : '+ msg);
+  });
+  
+  socket.on('disconnect', function() {
+    usercount --;
+    io.emit('message', socket.username + ' left the channel.');
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *: ' +port);
+});
