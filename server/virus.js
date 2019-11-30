d1ca4380f7f3facf5bc84532f026622307c17950
Nicolas Muguet <nicolas.muguet@capgemini.com>
2019-11-30 13:04:26 +0100

    feature/v1 : enhance virus movement

--- server/virus.js
+++ server/virus.js
@@ -16,6 +16,10 @@ function createVirus(numberToCreate) {
             id: ((new Date()).getTime() + '' + virusList.length) >>> 0,
             x: position.x,
             y: position.y,
+            direction_x : 0,
+            direction_y : 0,
+            changeDirectionFrequency : Math.floor(Math.random() * (config.ViruschangeDirectionFrequencyMax - config.ViruschangeDirectionFrequencyMin)+ config.ViruschangeDirectionFrequencyMin),
+            changeDirection : 0,
             radius: radius,
             mass: config.virusMass,
         });
@@ -30,20 +34,24 @@ function removeVirus(numberToRemove){
 }
 
 function moveVirus() {
-    var speed = config.virusSpeed
+    var speed = config.virusSpeed;
     virusList.forEach((virus) => {
-        const vertical = Math.random() < 0.5
-        const horizontal = Math.random() < 0.5
-        if ((virus.x + virus.radius) < (config.gameWidth - speed) && horizontal) {
+        if(virus.changeDirection == virus.changeDirectionFrequency){
+            virus.direction_x = Math.random() < 0.5;
+            virus.direction_y = Math.random() < 0.5;
+            virus.changeDirection = 0;
+        }
+        if ((virus.x + virus.radius) < (config.gameWidth - speed) && virus.direction_x) {
             virus.x += speed
         } else if ((virus.x - virus.radius) > (0 + speed)){
             virus.x -= speed
         }
-        if ((virus.y + virus.radius) < (config.gameHeight - speed) && vertical) {
+        if ((virus.y + virus.radius) < (config.gameHeight - speed) && virus.direction_y) {
             virus.y += speed
         } else if ((virus.y - virus.radius) > (0 + speed)){
             virus.y -= speed
         }
+        virus.changeDirection++;
     });
 }
 
