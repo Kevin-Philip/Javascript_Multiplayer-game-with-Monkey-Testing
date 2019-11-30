8efe335d2855cf9fdf2ad551dfda6763e7bb0ab5
Nicolas Muguet <nicolas.muguet@capgemini.com>
2019-11-30 20:44:25 +0100

    feature/v1: virus follow the player

--- server/virus.js
+++ server/virus.js
@@ -8,7 +8,7 @@ function createVirus(numberToCreate) {
     var radius = util.massToRadius(config.virusMass);
     while (numberToCreate > 0) {
         var position = util.randomPosition(radius);
-        while(util.isInContactWith(position, playerfile.playerList)){
+        while (util.isInContactWith(position, playerfile.playerList)) {
             console.log('[DEBUG] A virus spawned in a player')
             position = util.randomPosition(radius);
         }
@@ -16,10 +16,12 @@ function createVirus(numberToCreate) {
             id: ((new Date()).getTime() + '' + virusList.length) >>> 0,
             x: position.x,
             y: position.y,
-            direction_x : 0,
-            direction_y : 0,
-            changeDirectionFrequency : Math.floor(Math.random() * (config.VirusChangeDirectionFrequencyMax - config.VirusChangeDirectionFrequencyMin)+ config.VirusChangeDirectionFrequencyMin),
-            changeDirection : 0,
+            direction_x: 0,
+            direction_y: 0,
+            changeDirectionFrequency: Math.floor(Math.random() * (config.virusChangeDirectionFrequencyMax - config.virusChangeDirectionFrequencyMin) + config.virusChangeDirectionFrequencyMin),
+            changeDirection: 0,
+            changeTarget: 0,
+            target: playerfile.playerList[Math.floor(Math.random() * (playerfile.playerList.length))],
             radius: radius,
             mass: config.virusMass,
         });
@@ -27,8 +29,8 @@ function createVirus(numberToCreate) {
     }
 }
 
-function removeVirus(numberToRemove){
-    for (i = 0; i < numberToRemove; i++){
+function removeVirus(numberToRemove) {
+    for (i = 0; i < numberToRemove; i++) {
         virusList.pop();
     }
 }
@@ -36,22 +38,33 @@ function removeVirus(numberToRemove){
 function moveVirus() {
     var speed = config.virusSpeed;
     virusList.forEach((virus) => {
-        if(virus.changeDirection == virus.changeDirectionFrequency){
-            virus.direction_x = Math.random() < 0.5;
-            virus.direction_y = Math.random() < 0.5;
+        if (config.virusTracking && virus.changeTarget == config.virusChangeTargetFrequency) {
+            virus.target = playerfile.playerList[Math.floor(Math.random() * (playerfile.playerList.length))];
+            virus.changeTarget = 0;
+        }
+        if (virus.changeDirection == virus.changeDirectionFrequency) {
+            if (config.virusTracking) {
+                virus.direction_x = virus.x < virus.target.x;
+                virus.direction_y = virus.y < virus.target.y;
+            }
+            else {
+                virus.direction_x = Math.random() < 0.5;
+                virus.direction_y = Math.random() < 0.5;
+            }
             virus.changeDirection = 0;
         }
         if ((virus.x + virus.radius) < (config.gameWidth - speed) && virus.direction_x) {
             virus.x += speed
-        } else if ((virus.x - virus.radius) > (0 + speed)){
+        } else if ((virus.x - virus.radius) > (0 + speed)) {
             virus.x -= speed
         }
         if ((virus.y + virus.radius) < (config.gameHeight - speed) && virus.direction_y) {
             virus.y += speed
-        } else if ((virus.y - virus.radius) > (0 + speed)){
+        } else if ((virus.y - virus.radius) > (0 + speed)) {
             virus.y -= speed
         }
         virus.changeDirection++;
+        virus.changeTarget++;
     });
 }
 
