1e65ca8c8f1984f6427529d71643796ab55ce9bb
Kévin <kevin.philip@outlook.fr>
2020-01-18 16:56:47 +0100

    test(v1)

---
+++ server/test/app.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+
+import { expect } from 'chai';
+
+describe('Food tests', () => {
+  describe('createFood test', () => {
+    it('should create food', () => {
+      expect(1).to.equal(1);
+    });
+  });
+});
